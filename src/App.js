5ffb5a63b756283fa48281173f39ec8855463eba
ArC48 <beqa3352@gmail.com>
2023-12-14 17:58:01 +0400

    created pages

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import "./App.css";
 import Navigation from "./components/navigation";
 import {
     BrowserRouter as Router,
-    Switch,
     Route,
-    Redirect,
+    Routes,
+    Navigate,
 } from "react-router-dom";
+import PageRenderer from "./page-renderer";
 
 function App() {
     return (
         <Router>
             <div className="App">
                 <Navigation />
+                <Routes>
+                    <Route path="/:page" Component={PageRenderer} />
+                    <Route path="/" render={() => <Navigate to="/home" />} />
+                    <Route Component={() => 404} />
+                </Routes>
             </div>
         </Router>
     );
